a219ad1ade1c1b802dd060bfe11f4a6bfc683dc6
Derek Hall <Hderek22@icloud.com>
2023-02-01 22:34:29 -0500

    Built minting page
    -connect button
    -contact button
    -drag and drop window
    -mint button(wip)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import GemGrooveThumb from './GemGrooveThumb.jpg';
 
-function App() {
+  const LogoApp = () => {
+  const [isMetaMaskConnected, setIsMetaMaskConnected] = useState(false);
+  const [audioFile, setAudioFile] = useState(null);
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+
+    if (event.dataTransfer.files && event.dataTransfer.files.length) {
+      const file = event.dataTransfer.files[0];
+      setAudioFile(file);
+    }
+  };
+
+  const handleSave = () => {
+    // WIP - Need to send to NFT.Storage and return IPFS URL
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+  
+
+  const handleClick = async () => {
+    if (typeof window.ethereum === 'undefined') {
+      alert('Please install MetaMask to connect to your wallet.');
+      return;
+    }
+
+    try {
+      await window.ethereum.enable();
+      setIsMetaMaskConnected(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleMintClick = () => {
+    if (!isMetaMaskConnected) {
+      alert("Please connect to MetaMask first.");
+      return;
+    }
+    alert("Minting Jams soon...hang tight!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <img src={GemGrooveThumb} alt="GemGroove Thumb" />
+      <p style={{color: 'orange', marginTop: '20px'}}>Drag and Drop it like its hot!</p>
+      <input
+        type="text"
+        value={audioFile}
+        onDrop={handleDrop}
+        onDragOver={event => event.preventDefault()}
+        style={{width: '100px', height: '50px', border: '2px solid orange'}}
+      />
+      {audioFile ? (
+        <>
+          <p>{audioFile.name}</p>
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <p>Drop an audio file here</p>
+      )}
+      <button onClick={handleClick} style={{backgroundColor: 'orange', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '20px', position: 'fixed', bottom: '20px', left: '20px'}}>
+        Connect Wallet
+      </button>
+      <button onClick={handleMintClick} style={{backgroundColor: 'orange', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '5px', marginTop: '1px'}}>
+        Mint My Jam</button>
+        <button onClick={handleMintClick} style={{backgroundColor: 'orange', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '20px', marginTop: '20px', position: 'fixed',top:'90px'}}>
+        Our Purpose</button>
+      <a href="mailto:Hderek22@icloud.com">
+        <button style={{backgroundColor: 'orange', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '20px', marginTop: '20px', position: 'fixed', bottom: '20px', right: '20px'}}>
+          Contact GemGroove
+        </button>
+      </a>
     </div>
   );
-}
+};
 
-export default App;
+
+
+export default LogoApp;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { useState } from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const AudioUploader = () => {
+  const [audioFile, setAudioFile] = useState(null);
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+
+    if (event.dataTransfer.files && event.dataTransfer.files.length) {
+      const file = event.dataTransfer.files[0];
+      setAudioFile(file);
+    }
+  };
+
+  const handleSave = () => {
+    // Save the audio file to your desired location, using a back-end or a front-end library
+    // ...
+  };
+
+  return (
+    <div
+      onDrop={handleDrop}
+      onDragOver={event => event.preventDefault()}
+      style={{ backgroundColor: 'lightgray', height: '100px', textAlign: 'center' }}
+    >
+      {audioFile ? (
+        <>
+          <p>{audioFile.name}</p>
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <p>Drop an audio file here</p>
+      )}
+    </div>
+  );
+};
+
+export default AudioUploader;
